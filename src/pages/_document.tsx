5edab46af8e4351fb5a5e0f4e98661f173cd18dd
huishilhs <huishilhs@gmail.com>
2023-05-07 01:07:13 +0800

    buy sell listing page
    add buy sell listing page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import ResponsiveAppBar from '@/components/header'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <ResponsiveAppBar />
         <Main />
         <NextScript />
       </body>
